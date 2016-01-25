3f9cb770aae531b5f7e6a9398cb509a399e0db00
jmgpena <jorge@pond5.com>
2016-01-25 14:47:40 +0000

    Cleanup app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// app.js
+/* globals module require */
+'use strict';
 
 var path = require('path');
 var hapi = require('hapi');
@@ -43,6 +44,7 @@ server.route({
     }
 });
 
+/* serve static files on development */
 server.route({
     method: 'GET',
     path: '/{param*}',
@@ -53,42 +55,4 @@ server.route({
     }
 });
 
-server.start(() => {
-    console.log('Server runnint at:', server.info.uri);
-});
-
 module.exports = server;
-
-//var env = 'Unknown';
-
-// i18n.configure({
-//   locales: ['pt', 'en', 'es'],
-//   cookie: 'lang',
-//   defaultLocale: 'pt',
-//   directory: __dirname + '/locales'
-// });
-// app.set('view engine', 'jade');
-// // localization
-// app.use(cookieParser());
-// app.use(express.static('public'));
-// app.use(i18n.init);
-
-// app.get('/', function (req, res) {
-//     if (req.query.lang) {
-//         req.setLocale(req.query.lang);
-//         res.cookie('lang', req.query.lang);
-//     }
-//     res.render('index', { pageTitle: 'BBBF2016', message: res.__('Mensagem'), env: app.get('env') });
-// });
-
-// app.get('/cartaz', function (req, res) {
-//     res.render('index-cartaz');
-// });
-
-
-// var server = app.listen(3000, function () {
-//     var host = server.address().address;
-//     var port = server.address().port;
-
-//     console.log('bbbfng listening at http://%s:%s', host, port);
-// });
