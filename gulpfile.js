194fb01875b2ab41562bc9744b45d18c430c8762
jmgpena <jorge@pond5.com>
2016-02-01 14:13:31 +0000

    Require app.js on each server action.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var pm2 = require('pm2');
-var server = require('./app.js');
 
 var pm2Exec = function (action, data) {
     pm2.connect(function(err) {
@@ -47,20 +46,24 @@ var buildCss = function() {
 };
 
 gulp.task('server:start', function (callback) {
+    var server = require('./app.js');
     server.start(function() {
         callback();
     });
 });
 
 gulp.task('server:reload', (callback) => {
+    var server = require('./app.js');
     server.stop(() => {
         server.start(() => {
             callback();
+            browserSync.reload();
         });
     });
 });
 
 gulp.task('server:stop', function (callback) {
+    var server = require('./app.js');
     server.stop(function() {
         process.exit(0);
         callback();
@@ -72,7 +75,7 @@ gulp.task('development', ['server:start', 'css'], function() {
         proxy: {
             target: 'http://localhost:3000',
         },
-        files: ['views/*.jade','app.js'],
+        files: ['views/*.jade'],
         port: 7000
     });
 
