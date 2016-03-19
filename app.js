5e44bc525050a872796a38f543b6c0bd11775f16
jmgpena <jorge@pond5.com>
2016-03-19 21:44:32 +0000

    Code cleanup and refactoring
    Fix issue with pm2 watch reloading on log file changes!
    Move views inside app folder.

--- app.js
+++ app.js
@@ -7,67 +7,76 @@ const routes = require('./app/routes.js');
 const server = new hapi.Server();
 
 server.connection({
-    host: 'localhost',
-    port: 3000
+  host: 'localhost',
+  port: 3000
 });
 
 server.register(require('vision'), (err) => {
-    if (err) {
-        throw err;
+  if (err) {
+    throw err;
+  }
+  server.views({
+    engines: { jade: require('jade')},
+    path: __dirname + '/app/views',
+    isCached: false,
+    compileOptions: {
+      pretty: false
     }
-    server.views({
-        engines: { jade: require('jade')},
-        path: __dirname + '/views',
-        isCached: false,
-        compileOptions: {
-            pretty: false
-        }
-    });
+  });
 });
 
 server.register([
-    {
-        register: require('inert')
-    }
+  {
+    register: require('inert')
+  }
 ], (err) => {
-    if (err) {
-        console.error('Failed to load a plugin:', err);
-    }
+  if (err) {
+    console.error('Failed to load a plugin:', err);
+  }
 });
 
 server.register({
-    register: require('hapi-i18n'),
-    options: {
-        locales: ['pt', 'en', 'es'],
-        defaultLocale: 'pt',
-        directory: __dirname + '/locales'
-    }
+  register: require('hapi-i18n'),
+  options: {
+    locales: ['pt', 'en', 'es'],
+    defaultLocale: 'pt',
+    directory: __dirname + '/locales'
+  }
 }, (err) => {
-    if (err) {
-        console.error('Failed to load a plugin:', err);
-    }
+  if (err) {
+    console.error('Failed to load a plugin:', err);
+  }
 });
 
 // add i18n translation function to all jade requests
 server.ext('onPreResponse', (request, reply) => {
-    const response = request.response;
-    if (response.variety === 'view') {
-        let context = response.source.context;
-        context.__ = request.i18n.__;
-    }
-    return reply.continue();
+  const response = request.response;
+  if (response.variety === 'view') {
+    let context = response.source.context;
+    context.__ = request.i18n.__;
+  }
+  return reply.continue();
 });
 
 // add routes to server
 routes.map((route) => {
-    server.route(route);
+  server.route(route);
+});
+
+// server logging
+server.on('log', (event, tags) => {
+  if (tags.error) {
+    console.log(event);
+  }
 });
 
 // start server and send message to parent2
 server.start(() => {
-    console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri);
+  if ( typeof(process.send) == 'function' ) {
     process.send({
-        type: 'server:started',
-        data: {}
+      type: 'server:started',
+      data: {}
     });
+  }
 });
--- gulpfile.js
+++ gulpfile.js
@@ -5,81 +5,80 @@ const pm2 = require('pm2');
 // stop pm2 app on exiting gulp
 process.stdin.resume();
 process.on('SIGINT', () => {
-    console.log('exit:stopping server...');
-    pm2.connect((err) => {
-        if (err) {
-            console.error(err);
-            process.exit(2);
-        }
-        pm2.delete('bbbf-dev',() => {
-            process.exit(2);
-        });
+  console.log('exit:stopping server...');
+  pm2.connect((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(2);
+    }
+    pm2.delete('bbbf-dev',() => {
+      process.exit(2);
     });
+  });
 });
 
 // build the css
 var buildCss = function() {
-    var postcss    = require('gulp-postcss');
-    var sourcemaps = require('gulp-sourcemaps');
-    var processors = [
-        require('postcss-import'),
-        require('postcss-url'),
-        require('postcss-cssnext'),
-        require('postcss-browser-reporter'),
-        require('postcss-reporter')
-    ];
-    return gulp.src('css/**/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('public/stylesheets'));
+  var postcss    = require('gulp-postcss');
+  var sourcemaps = require('gulp-sourcemaps');
+  var processors = [
+    require('postcss-import'),
+    require('postcss-url'),
+    require('postcss-cssnext'),
+    require('postcss-browser-reporter'),
+    require('postcss-reporter')
+  ];
+  return gulp.src('css/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/stylesheets'));
 };
 
 gulp.task('development', ['css'], function() {
-    browserSync.init(null, {
-        proxy: {
-            target: 'http://localhost:3000',
-        },
-        //files: ['views/*.jade'],
-        port: 7000,
-        open: false
-    });
+  browserSync.init(null, {
+    proxy: {
+      target: 'http://localhost:3000',
+    },
+    //files: ['views/*.jade'],
+    port: 7000,
+    open: false
+  });
 
-    pm2.connect((err) => {
-        if (err) {
-            console.error(err);
-            process.exit(2);
-        }
-        pm2.start({
-            name: 'bbbf-dev',
-            script: 'app.js',
-            error_file: 'log/error.log',
-            out_file: 'log/out.log',
-            watch: true,
-            ignore_watch: ['node_modules', 'css', 'public', 'gulpfile.js', 'flycheck*', '.#*', '.git'],
-            env: {
-                NODE_ENV: 'development'
-            }
-        }, (err, apps) => {
-            pm2.disconnect();
-        });
-        pm2.launchBus((err, bus) => {
-            bus.on('server:started', () => {
-                console.log('[hapijs]: server started');
-                browserSync.reload();
-            });
-        });
+  pm2.connect((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(2);
+    }
+    pm2.start({
+      name: 'bbbf-dev',
+      script: 'app.js',
+      error_file: 'log/error.log',
+      out_file: 'log/out.log',
+      watch: ['app.js', 'app'],
+      env: {
+        NODE_ENV: 'development'
+      }
+    }, (err, apps) => {
+      pm2.disconnect();
     });
-    gulp.watch('css/**/*.css', ['dev-css']);
+    pm2.launchBus((err, bus) => {
+      bus.on('server:started', () => {
+        console.log('[hapijs]: server started');
+        browserSync.reload();
+      });
+    });
+  });
+  gulp.watch('css/**/*.css', ['dev-css']);
 });
 
 gulp.task('dev-css', function() {
-    buildCss().pipe(browserSync.stream({match: '**/*.css'}));
+  buildCss().pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 gulp.task('css', buildCss);
 
 gulp.task('default', ['development']);
 gulp.task('build', ['css'], () => {
-    process.exit(0);
+  process.exit(0);
 });
