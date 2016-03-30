46c515106a89c4ba6b732fc1d036669b11c7da04
jmgpena <jorge@pond5.com>
2016-03-30 14:09:40 +0100

    Add picture to news and update home page

--- app/actions/home.js
+++ app/actions/home.js
@@ -3,12 +3,13 @@ const db     = require('../db.js');
 // __: request.i18n.__,
 module.exports = function(request, reply) {
     let news;
-    db.select('title', 'body').from('news').limit(3)
+    db.select('title', 'body', 'pic').from('news').limit(3)
         .then((rows) => {
             console.log(rows);
             reply.view('index', {
                 message: request.i18n.__("Mensagem"),
-                news: rows
+                news: rows,
+                header: rows[0]
             });
         })
         .catch((error) => { console.log(error); });
