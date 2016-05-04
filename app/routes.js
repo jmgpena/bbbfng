bfe9e18230b28b619042c43bfcbe14d6e8b5b530
jmgpena <jmgpena@gmail.com>
2016-05-04 14:47:36 +0100

    Add unslider jquery plugin to homepage

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,28 @@
 'use strict';
 
 module.exports = [
+    // serve static files (should only apply on development)
+    { method: 'GET', path: '/js/{p*}',
+      handler: {
+          directory: {
+              path: 'public/js'
+          }
+      }
+    },
+    { method: 'GET', path: '/images/{p*}',
+      handler: {
+          directory: {
+              path: 'public/images'
+          }
+      }
+    },
+    { method: 'GET', path: '/stylesheets/{p*}',
+      handler: {
+          directory: {
+              path: 'public/stylesheets'
+          }
+      }
+    },
     // bands
     { method: 'GET', path: '/programa', handler: require('./actions/program.js') },
     { method: 'GET', path: '/{languageCode}/programa', handler: require('./actions/program.js') },
@@ -10,20 +32,5 @@ module.exports = [
     { method: 'GET', path: '/', handler: require('./actions/home.js') },
     { method: 'GET', path: '/{languageCode}/', handler: require('./actions/home.js') },
     { method: 'GET', path: '/{languageCode}', handler: require('./actions/home.js') },
-    // serve static files (should only apply on development)
-    { method: 'GET', path: '/images/{p*}',
-        handler: {
-            directory: {
-                path: 'public/images'
-            }
-        }
-    },
-    { method: 'GET', path: '/stylesheets/{p*}',
-      handler: {
-          directory: {
-              path: 'public/stylesheets'
-          }
-      }
-    },
     { method: '*', path: '/{p*}', handler: require('./actions/404.js') }
 ];
