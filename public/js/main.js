35ee99ef5f22cca2319166d845b6500dbde041fe
jmgpena <jorge@pond5.com>
2016-05-13 13:41:23 +0100

    Cleanup and finish setup of slider

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// Main browser js file
+
+jQuery(document).ready(function($) {
+    $('.my-slider').unslider({
+        arrows: false,
+        infinite: true,
+        autoplay: true
+    });
+});
